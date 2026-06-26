c69157478af655dbaa5b0fb870eb3e4cb648027e
Ankit kumar <ankurrt097@gmail.com>
2026-06-26 16:41:23 +0530

    Added email credentials feature for user creation

--- myproject/settings.py
+++ myproject/settings.py
@@ -111,10 +111,9 @@ LOGIN_URL = '/login/'
 # Email configuration
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 465
-
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 EMAIL_TIMEOUT = 10
 
